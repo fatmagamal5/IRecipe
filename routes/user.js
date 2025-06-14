0e14f35c717f7e8ff6528836e74d1d0c6ff4b4bf
julie <julie2300892@miuegypt.edu.eg>
2025-06-14 03:44:45 +0300

    user routes

---
+++ routes/user.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {
+  signup,
+  signin,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+  logout,
+  getFavorites,
+  addFavorite,
+  removeFavorite,
+  verifyUser,
+  changePassword,
+} from "../controllers/user.js";
+
+const router = express.Router();
+
+// Auth routes
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.get("/logout", logout);
+
+// Public user routes
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+
+// Protected user routes
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.put('/update', verifyUser, updateUser);
+router.delete('/delete', verifyUser, deleteUser);
+router.post('/change-password', verifyUser, changePassword);
+
+
+
+export default router;
